1c9171b53f9b83316251b4bb7aa8635b7288978c
14imran <14.imran.mohammed@gmail.com>
2024-02-05 14:07:25 -0600

    first commit

--- src/scenes/About/AboutTextCard/AboutTextCard.jsx
+++ src/scenes/About/AboutTextCard/AboutTextCard.jsx
@@ -8,7 +8,7 @@ const AboutTextCard = () => {
        Hola, I am{' '}
         <span className={s.purple}>Imran Mohammed </span>
         presently based in  <span className={s.purple}> Chicago, USA</span> <br />
-       With over a decade of expertise, I have a robust background in developing intricate web applications, utilizing technologies such as HTML, CSS, MySQL, JavaScript, XML, jQuery, AngularJS, Node.js, ReactJS, Vue.js, AJAX, and JSON. Proficient in DOM manipulation through JavaScript, jQuery, and AngularJS, I excel in creating responsive web and mobile designs using different Ui/Ux tools like figma,adobe. My skill set extends to HTML5, CSS, Bootstrap, JavaScript, jQuery, JSON, XML, AJAX, AngularJS, ReactJS, RequireJS, NodeJS, ECMAScript 6 (ES6), and TypeScript. Additionally, I have hands-on experience with ReactJS, React Flux, and Redux architectures, implementing complex Object-Oriented concepts to enhance website performance.
+       With over a decade of expertise, I have a robust background in developing  web applications, utilizing technologies such as HTML, CSS, MySQL, JavaScript, XML, jQuery, AngularJS, Node.js, ReactJS, Vue.js, AJAX, and JSON. Proficient in DOM manipulation through JavaScript, jQuery, and AngularJS, I excel in creating responsive web and mobile designs using different Ui/Ux tools like figma,adobe. My skill set extends to HTML5, CSS, Bootstrap, JavaScript, jQuery, JSON, XML, AJAX, AngularJS, ReactJS, RequireJS, NodeJS, ECMAScript 6 (ES6), and TypeScript. Additionally, I have hands-on experience with ReactJS, React Flux, and Redux architectures, implementing complex Object-Oriented concepts to enhance website performance.
  
       </p>
 
